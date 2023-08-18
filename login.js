cdc657a14d53c0085217e8571af13db074d34966
Md.Samsul Arefin <samsularefin532@gmail.com>
2023-08-18 22:20:02 +0600

    pass

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.getElementById('btn-submit').addEventListener('click', function () {
         window.location.href = 'user_web_view.html';
     }
     else {
-        alert('dont dream!!');
+        alert('chaprabo bainchod !!');
         document.getElementById('password').value = '';
         document.getElementById('email').value = '';
     }
